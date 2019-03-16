6edbae3ce188d5e6c3d2df71fcca0676fd21febb
j4sysiak <j4sysiak@gmail.com>
2019-03-16 17:22:30 +0100

    112

--- spring-tutorial-Web-Application-Basics-with-Spring-MVC-56/src/main/java/pl/jaceksysiak/web/controllers/OffersController.java
+++ spring-tutorial-Web-Application-Basics-with-Spring-MVC-56/src/main/java/pl/jaceksysiak/web/controllers/OffersController.java
@@ -1,13 +1,8 @@
 package pl.jaceksysiak.web.controllers;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OffersController {
